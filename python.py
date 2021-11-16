b3b4ad77a4b640a2460408181ca3811a3b4ae31d
makarosca <93660259+makarosca@users.noreply.github.com>
2021-11-16 14:11:53 +0100

    Update python.py
    Extracting NGS reads and dividing into k-mers, 
    extracting resistance genes and dividing into k-mers
    checking if there is at least one match between the k-mers of an NGS read and k-mers of all genes

--- python.py
+++ python.py
@@ -1,82 +1,93 @@
 #!/usr/bin/env python3
 
-#open the resistance gene file
-	#extract all genes into a dict variable
-	
-
-#open gzip file
-#for line in file
-	#find the sequence between @ and + lines
-	#extract all 19-mers into a list
-
-	#for 
-
-
-""" ### Just playing with 'gzip' library
-
+import sys
 import gzip
 
-with gzip.open('file.txt.gz', 'w') as fileout:
-	for i in range(10):
-		# has to be a byte string -- b'something something'
-		fileout.write(b"Oh that's line number ")
-		# convert number into string so I can encode it into bytes
-		fileout.write(str(i).encode('ASCII'))
-		fileout.write(b'\n')
+def read_filename():		
+	# read the filename
+	if len(sys.argv) == 2:
+		filename = sys.argv[-1]
+	else:
+		filename = input('Enter <filename>: ')
+	return filename 
 
-with gzip.open('file.txt.gz', 'r') as filein:
-	for line in filein:
-		print(line[:-1].decode('ASCII'))
-
-s_in = b"Lots of content here"
-s_out = gzip.compress(s_in)
-
-print(s_out)
-
-"""
-
-import gzip
-
-def get_dnaread(filename):
-	### Extracts a DNA line from the next NGS read in the file
-
-	flag = False
-
-	# reads 4 lines i.e. the whole NGS read
-	for j in range(4):
-		# read line and decode from bytes
-		line = filein.readline().decode('ASCII')
-
-		# I thought we could use stateful parsing to make suuuuuure 
-		# that we get the DNA line and not something else, or is it stupid?
-			# other ideas for input control: regex
-			# and of course add try-except
-				# for example if flag is still True in the end 
-				# of function -- something wrong
-
-		if line[0] == '@':
-			flag = True
-		elif flag:
-			if line[0] == '+':
-				flag = False
+def get_genes(filename):
+	with open(filename, 'r') as filein:
+		genes = []
+		dna = ''
+		for line in filein:
+			if line[0] == '>':
+				if dna != '':
+					genes.append(dna)
+				dna = ''
 			else:
-				dna = line
+				dna += line[:-1]
+	return genes
 
-	return dna
-	# return a signal if reached end of file
+def get_gene_kmer(genes, k):
+	### returns a set of all k-mers taken from all resistance genes
+	gene_kmer = set()
+	for gene in genes:
+		for i in range(len(gene)-k+1):		# len-k+1 takes the last symbol too
+			gene_kmer.add(gene[i:i+k])		# [i:i+19] -- 19-mer
+	return gene_kmer
+
+def get_ngsread_kmer_list(dna, k):
+	### returns a LIST of all k-mers extracted from an NGS read in the 
+	### respective order
+
+	ngsread_kmer = []
+	for i in range(len(dna)-k):				# len-k ignores \n at the end
+		ngsread_kmer.append(dna[i:i+k])
+	return ngsread_kmer
+
+
+def get_ngsread_kmer_set(dna, k):
+	### returns a SET of all k-mers extracted from an NGS read
+
+	ngsread_kmer = set()
+	for i in range(len(dna)-k):
+		ngsread_kmer.add(dna[i:i+k])
+	return ngsread_kmer
+
 
 
 ### Main Program ###
 
-filein = gzip.open('Unknown3_raw_reads_1.txt.gz', 'r')
+k = 19					# k-mer size
 
-dnaread = get_dnaread(filein)
-# in the end switch the loop to 'while not end-of-file'
-for i in range(5):					# get top 5 DNA entries
-	print(dnaread)
-	dnaread = get_dnaread(filein)
-print(i)
-filein.close()
+gene_list = get_genes('resistance_genes.fsa')
+gene_kmer = get_gene_kmer(gene_list, k)
+print(gene_list[0], len(gene_kmer))
+count = 0					# counts dna reads in the NGS file
+countin = 0					# counts dna reads with at least one k-mer match
 
-# I tried 1 million DNA reads without printing them -- it takes 7 seconds heheh
+filename = read_filename()
+filein = gzip.open(filename, 'r')
+for b_line in filein:
+	line = b_line.decode('ASCII')
 
+	if line[0] == '@':
+		flag = True
+	elif flag:
+		if line[0] == '+':
+			flag = False
+
+		else:
+			# get the dna seq
+			dna_read = line
+			count += 1
+
+			# check if at least 1 k-mer from read is present in genes
+			ngsread_kmer = get_ngsread_kmer_list(dna_read, k)
+			for kmer in ngsread_kmer:
+				if kmer in gene_kmer:
+					countin += 1
+					break					# takes 2m9s
+
+#			ngsread_kmer = get_ngsread_kmer_set(dna_read, k)		
+#			if len( ngsread_kmer.intersection(gene_kmer) ) > 0:		# or > 1?
+#				countin += 1				# takes 1m59.9s
+#				ngsread_kmer = get_ngsread_kmer_list(dna_read, k)	# takes 2m9s
+
+print(countin, '/', count)					# 6,717 / 3,469,171			
