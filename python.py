b31c915e59b571451b3913e4ef2febec53ef472d
makarosca <93660259+makarosca@users.noreply.github.com>
2021-11-22 01:42:40 +0100

    Update python.py
    Now we receive a matrix 'depth_update' from the function 'match_ngs_to_gene'
    The matrix (zeroes and ones) is the info where the read was aligned to the gene (one)
    
    It is super slow, takes a 2.5 min to go through 3.5% of reads
    
    Next idea: 
    intersection of sets of genes and k-mers from reads
    Predetermine the list of genes to analyze, don't analyze ALL genes EVERY read
    Analyze only genes which contain at least one k-mer match

--- python.py
+++ python.py
@@ -14,15 +14,19 @@ def read_filename():
 def get_genes(filename):
 	with open(filename, 'r') as filein:
 		genes = []
+		gene_names = []
 		dna = ''
+		poop = []
 		for line in filein:
 			if line[0] == '>':
+				gene_names.append(line[:-1])
 				if dna != '':
 					genes.append(dna)
 				dna = ''
 			else:
 				dna += line[:-1]
-	return genes
+		genes.append(dna)
+	return [genes, gene_names]
 
 def get_gene_kmer(genes, k):
 	### returns a set of all k-mers taken from all resistance genes
@@ -50,15 +54,68 @@ def get_ngsread_kmer_set(dna, k):
 		ngsread_kmer.add(dna[i:i+k])
 	return ngsread_kmer
 
+def match_ngs_to_gene(ngsread_kmer, gene):
+	### returns the list of depths of nucleotides that matched for the gene
+	match = False
+
+	for i in range(len(ngsread_kmer)):
+		k = len(ngsread_kmer[0])			# k as in k-mer, usually 19
+
+		# see if the k-mer is found in the gene and find its pos
+		start_gene = gene.find(ngsread_kmer[i])	
+		if start_gene != -1:			# -1 if the k-mer is not found
+			start_read = i
+
+			if start_gene > 0:
+				# start_read must be 0
+				if start_read == 0:
+					
+					# list with zeroes, length = len(gene)
+					depth = [0 for j in range(len(gene))]
+					print('before',len(depth))				####### print for visualization, delete later
+					# depth = 1 for the first matched k-mer
+					depth[start_gene:start_gene+k] = [1 for j in range(k)]
+
+					pos_read = start_read + 1
+					pos_gene = start_gene + 1
+					while pos_read < len(ngsread_kmer) and pos_gene < len(gene)-k:
+						if ngsread_kmer[pos_read] == gene[pos_gene:pos_gene+k]:
+							depth[pos_gene:pos_gene+k] = [1 for j in range(k)]		# optimize later?
+							#depth[pos_gene+k-1] = 1
+							pos_read += k
+							pos_gene += k
+						
+						else:
+							print('after',len(depth))		####### print for visualization, delete later
+							return depth
+
+
+
+
+				else:
+					return None
+
+			elif start_gene == 0:
+				pass
+
+
+
+	return None
+
+
+#	while kmer[startpos] not in gene_list:
+#		startpos += 1
+#		if startpos == 
+
 
 
 ### Main Program ###
 
-k = 19					# k-mer size
+k = 19					# k-mer size, usually 19
 
-gene_list = get_genes('resistance_genes.fsa')
+[gene_list, gene_names] = get_genes('resistance_genes.fsa')
 gene_kmer = get_gene_kmer(gene_list, k)
-print(gene_list[0], len(gene_kmer))
+#print(gene_list[0], len(gene_kmer))
 count = 0					# counts dna reads in the NGS file
 countin = 0					# counts dna reads with at least one k-mer match
 
@@ -80,9 +137,15 @@ for b_line in filein:
 
 			# check if at least 1 k-mer from read is present in genes
 			ngsread_kmer = get_ngsread_kmer_list(dna_read, k)
+
 			for kmer in ngsread_kmer:
 				if kmer in gene_kmer:
 					countin += 1
+					for gene_num in range(len(gene_list)):
+						depth_update = match_ngs_to_gene(ngsread_kmer, gene_list[gene_num])
+						if depth_update is not None:
+							print(gene_names[gene_num], '|', countin, '| Read #', count)		####### print for visualization, delete later
+							print(depth_update, '\n')						####### print for visualization, delete later
 					break					# takes 2m9s
 
 #			ngsread_kmer = get_ngsread_kmer_set(dna_read, k)		
