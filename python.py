d2efc67d7020d128f81a20d49512c4e427971e3a
makarosca <93660259+makarosca@users.noreply.github.com>
2021-11-29 12:48:40 +0100

    Update python.py

--- python.py
+++ python.py
@@ -1,93 +1,233 @@
 #!/usr/bin/env python3
+'''
+Res genes: Dict, key = kmer, value = positions CHECK
+Then compare kmers against it (first and last of the read)
+Two files need to work at a time!
+Create reverse complementary strands of reads!
 
+To possibly improve:
+in read_filename() make it possible for as many files to be added as we want
+'''
 import sys
 import gzip
 
 def read_filename():		
 	# read the filename
-	if len(sys.argv) == 2:
-		filename = sys.argv[-1]
+	if len(sys.argv) == 3:
+		f1 = sys.argv[-2]
+		f2 = sys.argv[-1]
 	else:
-		filename = input('Enter <filename>: ')
-	return filename 
-
-def get_genes(filename):
-	with open(filename, 'r') as filein:
-		genes = []
-		dna = ''
-		for line in filein:
-			if line[0] == '>':
-				if dna != '':
-					genes.append(dna)
-				dna = ''
-			else:
-				dna += line[:-1]
-	return genes
-
-def get_gene_kmer(genes, k):
-	### returns a set of all k-mers taken from all resistance genes
-	gene_kmer = set()
-	for gene in genes:
-		for i in range(len(gene)-k+1):		# len-k+1 takes the last symbol too
-			gene_kmer.add(gene[i:i+k])		# [i:i+19] -- 19-mer
-	return gene_kmer
-
+		f1 = input('Enter <filename>: ')
+		f2 = input('Enter another <filename>: ')
+	filenames = [f1, f2]
+	return filenames
+	
+	
 def get_ngsread_kmer_list(dna, k):
 	### returns a LIST of all k-mers extracted from an NGS read in the 
 	### respective order
 
 	ngsread_kmer = []
-	for i in range(len(dna)-k):				# len-k ignores \n at the end
+	for i in range(len(dna)-k+1):				# len-k ignores \n at the end
 		ngsread_kmer.append(dna[i:i+k])
 	return ngsread_kmer
+	
+def get_genes(filename):
+	with open(filename, 'r') as infile:
+		genes = []
+		gene_names = []
+		dna = ''
+		for line in infile:
+			if line[0] == '>':
+				gene_names.append(line[:-1])
+				if dna != '':
+					genes.append(dna)
+				dna = ''
+			else:
+				dna += line[:-1]
+		genes.append(dna)
+	return genes, gene_names
 
 
-def get_ngsread_kmer_set(dna, k):
-	### returns a SET of all k-mers extracted from an NGS read
+def complementary_strand(read):
+	### returns a reverse complementary version of the read
+	transTable = str.maketrans('ATCG', 'TAGC')
+	complementDNA = read.translate(transTable)
+	rev_dna = complementDNA[::-1]  
+	
+	return rev_dna
 
-	ngsread_kmer = set()
-	for i in range(len(dna)-k):
-		ngsread_kmer.add(dna[i:i+k])
-	return ngsread_kmer
+#a = read_filename()	#delete
+#print(a[1])			#delete
+
+def get_gene_kmer(genes, k):
+	### returns a set of all k-mers taken from all resistance genes
+	gene_kmer_dict = dict()
+	
+	for gene_num in range(len(genes)):
+		for i in range( len(genes[gene_num]) -k+1):		# len-k+1 takes the last symbol too
+			check = gene_kmer_dict.get(genes[gene_num][i:i+k])
+			gene_kmer_dict[genes[gene_num][i:i+k]] = {gene_num: i}	# value = kmer position [which gene][where in the gene]
+
+	return gene_kmer_dict
 
 
 
-### Main Program ###
+'''MAIN CODE'''
+	
+k = 19 #setting the kmer length
 
-k = 19					# k-mer size
+###processing resistance genes
 
-gene_list = get_genes('resistance_genes.fsa')
-gene_kmer = get_gene_kmer(gene_list, k)
-print(gene_list[0], len(gene_kmer))
-count = 0					# counts dna reads in the NGS file
-countin = 0					# counts dna reads with at least one k-mer match
+#Obtaining lists of resistance gene sequences and names
+[gene_list, gene_names] = get_genes('resistance_genes.fsa')
 
-filename = read_filename()
-filein = gzip.open(filename, 'r')
-for b_line in filein:
-	line = b_line.decode('ASCII')
+#Obtaining a dict of res_gene k-mers with their respective locations as values
+# Dict of dicts: gene_dict [k-mer sequence] [gene_num] == position of the k-mer in the specific gene
+gene_dict = get_gene_kmer(gene_list, k)
 
-	if line[0] == '@':
-		flag = True
-	elif flag:
-		if line[0] == '+':
-			flag = False
+###creating a template for output
+#Creating depth list
+depth = []
+for gene_num in range(len(gene_list)):
+	d1 = [0 for j in range(len(gene_list[gene_num])) ]
+	depth.append(d1)
+	#for j in range(len(gene_list[gene_num])):
+	#	depth[gene_num].append(0)
 
-		else:
-			# get the dna seq
-			dna_read = line
-			count += 1
 
-			# check if at least 1 k-mer from read is present in genes
-			ngsread_kmer = get_ngsread_kmer_list(dna_read, k)
-			for kmer in ngsread_kmer:
-				if kmer in gene_kmer:
-					countin += 1
-					break					# takes 2m9s
+###processing the reads
 
-#			ngsread_kmer = get_ngsread_kmer_set(dna_read, k)		
-#			if len( ngsread_kmer.intersection(gene_kmer) ) > 0:		# or > 1?
-#				countin += 1				# takes 1m59.9s
-#				ngsread_kmer = get_ngsread_kmer_list(dna_read, k)	# takes 2m9s
+#opening both read files
+filenames = read_filename()
+read_count = 0
+for file in filenames:		#to iterate through both files
+	with gzip.open(file, "r") as infile:
+		for b_line in infile:
+			line = b_line.decode('ASCII')
 
-print(countin, '/', count)					# 6,717 / 3,469,171			
+			if line[0] == '@':				# getting the DNA
+				flag = True					# sequence through
+			elif flag:						# stateful parsing
+				if line[0] == '+':
+					flag = False
+				else:
+					read_count += 1
+					dna_read = line[:-1]
+					#get the reverse complement for the read
+					rev_read = complementary_strand(dna_read)
+					both_reads = [dna_read, rev_read]
+
+					for read in both_reads:			# iterate through both strands
+						#create kmer lists for both
+						read_kmer = get_ngsread_kmer_list(read, k)
+
+						#checking if extremities of the read fit the dict (initial read elimination)
+						start_check = gene_dict.get(read_kmer[0])			# contain positions of the k-mers 
+						end_check = gene_dict.get(read_kmer[-1])			# in every relevant gene
+						
+						# list of numbers of genes to which the read will be aligned
+						genes_to_analyze = set()
+						if start_check is not None:
+							for hit in start_check.keys():
+								genes_to_analyze.add(hit)
+						if end_check is not None:
+							for hit in end_check.keys():
+								genes_to_analyze.add(hit)
+
+						
+						# iterate through every relevant gene
+						for gene_num in genes_to_analyze:
+							SNP = 0
+							update_reg = None
+
+							if start_check is not None and start_check.get(gene_num) is not None:
+								
+								if end_check is not None and end_check.get(gene_num) is not None:
+
+									# end_check[gene_num] is the position of beginning of the last k-mer
+									# hence 'end_check[gene_num]+k' is the next nucleotide after the gene part
+									part_of_gene = gene_list[gene_num][ start_check[gene_num]:end_check[gene_num]+k ]
+
+									# if both first and last k-mer of the read ARE present in the gene, align the whole read
+									if read == part_of_gene:
+										update_reg = [start_check[gene_num], end_check[gene_num]+k]
+									
+									# if the read is not identical to the part of gene, allow one SNP difference
+									elif len(read) == len(part_of_gene):
+										i = 0 			# number of the k-mer (position in the read)
+										pos = 0 		# position in the gene sequence
+										while i < len(read_kmer) and pos < len(gene_list[gene_num]):
+											pos = start_check[gene_num] + i
+											if read_kmer[i] != gene_list[gene_num][pos:pos+k]:
+												SNP += 1
+												i += k
+											else:
+												i += 1
+										if SNP == 1:
+											update_reg = [start_check[gene_num], end_check[gene_num]+k]
+										
+										# discard if not identical and SNP > 1
+										else:
+											update_reg = None
+									else:
+										update_reg = None
+
+								# if only the first k-mer of the read is found, align to the end of gene			
+								else:
+									gene_end = gene_list[gene_num][-k:]
+									end_pos = read.find(gene_end)
+									part_of_gene = gene_list[gene_num][ start_check[gene_num]:]
+									if end_pos != -1 and read[:end_pos+k] == part_of_gene:
+										update_reg = [start_check[gene_num], len(gene_list[gene_num])]
+									else:
+										update_reg = None
+
+							# if only the last k-mer of the read is found, align to the beginning of gene
+							else:
+								if end_check is not None and end_check.get(gene_num) is not None:
+									gene_start = gene_list[gene_num][:k]
+									start_pos = read.find(gene_start)
+									part_of_gene = gene_list[gene_num][:end_check[gene_num]+k]
+									if start_pos != -1 and read[start_pos:] == part_of_gene:
+										update_reg = [0, end_check[gene_num]+k]
+									else:
+										update_reg = None
+								else:
+									update_reg = None
+
+							# if the read is not discarded, increase the depth of 
+							# gene by 1 in relevant region
+							if update_reg is not None:
+								print("{:.2f}".format(read_count/6938342*100), '%', sep='')
+								for i in range(update_reg[0], update_reg[1]):
+									depth[gene_num][i] += 1
+									
+
+# calculate coverage and average
+coverageCount = 0 			#
+coverage = dict()
+avg_depth = []
+for gene_num in range(len(depth)):
+	hitCount = 0
+	avg_depth.append(0)
+	for i in range(len(depth[gene_num])):
+		if depth[gene_num][i] >= 10:
+			hitCount += 1
+		avg_depth[gene_num] += depth[gene_num][i]
+	a = hitCount/len(depth[gene_num])
+	avg_depth[gene_num] = avg_depth[gene_num]/len(depth[gene_num])
+	if a > 0.95:
+		coverageCount += 1
+	coverage[gene_num] = a
+
+sorted_gene_num = sorted(coverage.keys(), key=coverage.get, reverse=True)
+print(coverageCount, " genes have achieved coverage above 95%:", sep='')
+i = 0
+for gene_num in sorted_gene_num[:coverageCount]:
+	i += 1
+	# display the gene names that are covered, their coverage and average depth
+	print(i, '. ', gene_names[gene_num], sep='');
+	print("Gene coverage: ", "{:.2f}".format(coverage[gene_num]*100), "%; Gene depth: ", 
+		"{:.2f}".format(avg_depth[gene_num]), sep='')	
+	#print(depth[gene_num])						#To see corresponding depth distribution 
