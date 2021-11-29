d71041a820be3be5140377301730acf3312ef5d4
makarosca <93660259+makarosca@users.noreply.github.com>
2021-11-29 15:57:15 +0100

    Update python.py

--- python.py
+++ python.py
@@ -71,11 +71,75 @@ def get_gene_kmer(genes, k):
 
 	return gene_kmer_dict
 
+def align_ngs_gene(read, gene, start_check, end_check, gene_num, k):
+	### returns the region of the gene where the read was successfully aligned, otherwise returns None
+
+	SNP = 0
+	update_reg = None
+
+	if start_check is not None and start_check.get(gene_num) is not None:
+		
+		if end_check is not None and end_check.get(gene_num) is not None:
+
+			# end_check[gene_num] is the position of beginning of the last k-mer
+			# hence 'end_check[gene_num]+k' is the next nucleotide after the gene part
+			part_of_gene = gene[ start_check[gene_num]:end_check[gene_num]+k ]
+
+			# if both first and last k-mer of the read ARE present in the gene, align the whole read
+			if read == part_of_gene:
+				update_reg = [start_check[gene_num], end_check[gene_num]+k]
+			
+			# if the read is not identical to the part of gene, allow one SNP difference
+			elif len(read) == len(part_of_gene):
+				i = 0 			# number of the k-mer (position in the read)
+				pos = 0 		# position in the gene sequence
+				while i < len(read_kmer) and pos < len(gene):
+					pos = start_check[gene_num] + i
+					if read_kmer[i] != gene[pos:pos+k]:
+						SNP += 1
+						i += k
+					else:
+						i += 1
+				if SNP == 1:
+					update_reg = [start_check[gene_num], end_check[gene_num]+k]
+				
+				# discard if not identical and SNP > 1
+				else:
+					update_reg = None
+			else:
+				update_reg = None
+
+		# if only the first k-mer of the read is found, align to the end of gene			
+		else:
+			gene_end = gene[-k:]
+			end_pos = read.find(gene_end)
+			part_of_gene = gene[ start_check[gene_num]:]
+			if end_pos != -1 and read[:end_pos+k] == part_of_gene:
+				update_reg = [start_check[gene_num], len(gene)]
+			else:
+				update_reg = None
+
+	# if only the last k-mer of the read is found, align to the beginning of gene
+	else:
+		if end_check is not None and end_check.get(gene_num) is not None:
+			gene_start = gene[:k]
+			start_pos = read.find(gene_start)
+			part_of_gene = gene[:end_check[gene_num]+k]
+			if start_pos != -1 and read[start_pos:] == part_of_gene:
+				update_reg = [0, end_check[gene_num]+k]
+			else:
+				update_reg = None
+		else:
+			update_reg = None
+
+	return update_reg
+
+
 
 
 '''MAIN CODE'''
 	
-k = 19 #setting the kmer length
+k = 19 #setting the k-mer length
 
 ###processing resistance genes
 
@@ -92,8 +156,6 @@ depth = []
 for gene_num in range(len(gene_list)):
 	d1 = [0 for j in range(len(gene_list[gene_num])) ]
 	depth.append(d1)
-	#for j in range(len(gene_list[gene_num])):
-	#	depth[gene_num].append(0)
 
 
 ###processing the reads
@@ -138,75 +200,23 @@ for file in filenames:		#to iterate through both files
 						
 						# iterate through every relevant gene
 						for gene_num in genes_to_analyze:
-							SNP = 0
-							update_reg = None
 
-							if start_check is not None and start_check.get(gene_num) is not None:
-								
-								if end_check is not None and end_check.get(gene_num) is not None:
-
-									# end_check[gene_num] is the position of beginning of the last k-mer
-									# hence 'end_check[gene_num]+k' is the next nucleotide after the gene part
-									part_of_gene = gene_list[gene_num][ start_check[gene_num]:end_check[gene_num]+k ]
-
-									# if both first and last k-mer of the read ARE present in the gene, align the whole read
-									if read == part_of_gene:
-										update_reg = [start_check[gene_num], end_check[gene_num]+k]
-									
-									# if the read is not identical to the part of gene, allow one SNP difference
-									elif len(read) == len(part_of_gene):
-										i = 0 			# number of the k-mer (position in the read)
-										pos = 0 		# position in the gene sequence
-										while i < len(read_kmer) and pos < len(gene_list[gene_num]):
-											pos = start_check[gene_num] + i
-											if read_kmer[i] != gene_list[gene_num][pos:pos+k]:
-												SNP += 1
-												i += k
-											else:
-												i += 1
-										if SNP == 1:
-											update_reg = [start_check[gene_num], end_check[gene_num]+k]
-										
-										# discard if not identical and SNP > 1
-										else:
-											update_reg = None
-									else:
-										update_reg = None
-
-								# if only the first k-mer of the read is found, align to the end of gene			
-								else:
-									gene_end = gene_list[gene_num][-k:]
-									end_pos = read.find(gene_end)
-									part_of_gene = gene_list[gene_num][ start_check[gene_num]:]
-									if end_pos != -1 and read[:end_pos+k] == part_of_gene:
-										update_reg = [start_check[gene_num], len(gene_list[gene_num])]
-									else:
-										update_reg = None
-
-							# if only the last k-mer of the read is found, align to the beginning of gene
-							else:
-								if end_check is not None and end_check.get(gene_num) is not None:
-									gene_start = gene_list[gene_num][:k]
-									start_pos = read.find(gene_start)
-									part_of_gene = gene_list[gene_num][:end_check[gene_num]+k]
-									if start_pos != -1 and read[start_pos:] == part_of_gene:
-										update_reg = [0, end_check[gene_num]+k]
-									else:
-										update_reg = None
-								else:
-									update_reg = None
+							# region that has to be updated
+							update_reg = align_ngs_gene(read, gene_list[gene_num], 
+								start_check, end_check, gene_num, k)
 
 							# if the read is not discarded, increase the depth of 
 							# gene by 1 in relevant region
-							if update_reg is not None:
-								print("{:.2f}".format(read_count/6938342*100), '%', sep='')
+							if update_reg is not None:			
 								for i in range(update_reg[0], update_reg[1]):
 									depth[gene_num][i] += 1
+								# printing percentage of the reads processed
+								print("{:.2f}".format(read_count/6938342*100), '%', sep='')
 									
 
 # calculate coverage and average
-coverageCount = 0 			#
-coverage = dict()
+coverageCount = 0 			# number of genes with coverage > 95%
+coverage = dict()			# coverage of each gene
 avg_depth = []
 for gene_num in range(len(depth)):
 	hitCount = 0
@@ -215,19 +225,18 @@ for gene_num in range(len(depth)):
 		if depth[gene_num][i] >= 10:
 			hitCount += 1
 		avg_depth[gene_num] += depth[gene_num][i]
-	a = hitCount/len(depth[gene_num])
+	coverage[gene_num] = hitCount/len(depth[gene_num])
 	avg_depth[gene_num] = avg_depth[gene_num]/len(depth[gene_num])
-	if a > 0.95:
+	if coverage[gene_num] > 0.95:
 		coverageCount += 1
-	coverage[gene_num] = a
 
 sorted_gene_num = sorted(coverage.keys(), key=coverage.get, reverse=True)
-print(coverageCount, " genes have achieved coverage above 95%:", sep='')
+print(coverageCount, " genes have achieved coverage above 95%:\n", sep='')
 i = 0
 for gene_num in sorted_gene_num[:coverageCount]:
 	i += 1
 	# display the gene names that are covered, their coverage and average depth
 	print(i, '. ', gene_names[gene_num], sep='');
 	print("Gene coverage: ", "{:.2f}".format(coverage[gene_num]*100), "%; Gene depth: ", 
-		"{:.2f}".format(avg_depth[gene_num]), sep='')	
-	#print(depth[gene_num])						#To see corresponding depth distribution 
+		"{:.2f}".format(avg_depth[gene_num]), '\n', sep='')	
+	#print(depth[gene_num])						# To see corresponding depth distribution 
